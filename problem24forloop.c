4aed098bb80b381f69bf16fc7625372e71685b6e
Your Name <strokeofdeath8540@gmail.com>
2021-04-24 23:31:01 +0530

    Update problem24forloop.c

--- problem24forloop.c
+++ problem24forloop.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include<stdio.h> /*This code is to print a right angle triangle pattern of natural repeating numbers.*/
 int main()
 {
     int i,j,row;
 
-    printf("Enter your value : ");
+    printf("Enter your value: ");
     scanf("%d",&row);
 
     for (i=1;i<=row;i++)
